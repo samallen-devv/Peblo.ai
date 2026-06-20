4000077869e448575a0a9f1a5beb34ef5041b9e8
samallen-devv <mohdsami.dev@gmail.com>
2026-06-20 23:48:33 +0530

    creating story card widget

---
+++ lib/features/story_buddy/presentation/widgets/story_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class StoryCard extends StatelessWidget {
+  final String story;
+
+  const StoryCard({
+    super.key,
+    required this.story,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.08),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Text(
+        story,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          fontSize: 18,
+          height: 1.6,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
