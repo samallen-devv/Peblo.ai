511a6f7594211c475d798f86c5c3c6d5a3df83ac
samallen-devv <mohdsami.dev@gmail.com>
2026-06-20 23:46:16 +0530

    add constant app string file

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,18 @@
+class AppStrings {
+  static const String appTitle = 'Peblo AI Story Buddy';
+
+  static const String readStory = 'Read Me A Story';
+
+  static const String loadingStory = 'Preparing Story...';
+
+  static const String speaking = 'Reading Story...';
+
+  static const String retry = 'Try Again';
+
+  static const String success = 'Amazing Job!';
+
+  static const String successMessage = 'You helped Pip find his blue gear!';
+
+  static const String storyText =
+      'Once upon a time, a clever little robot named Pip lost his shiny blue gear in the Whispering Woods...';
+}
