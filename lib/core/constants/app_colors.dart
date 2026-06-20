4da20bf70904008b36da7d767800908ca15868a5
samallen-devv <mohdsami.dev@gmail.com>
2026-06-20 23:45:46 +0530

    add constant color file

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF8B5CF6);
+
+  static const Color secondary = Color(0xFFFFB703);
+
+  static const Color background = Color(0xFFF7F3FF);
+
+  static const Color cardBackground = Colors.white;
+
+  static const Color success = Color(0xFF4CAF50);
+
+  static const Color error = Color(0xFFE53935);
+
+  static const Color storyCard = Color(0xFFEDE9FE);
+
+  static const Color quizCard = Color(0xFFFFF8E1);
+
+  static const Color buttonBlue = Color(0xFF4FC3F7);
+
+  static const Color textDark = Color(0xFF2D3748);
+}
